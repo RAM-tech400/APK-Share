527c66df703a488db87a00f9bd3aa228ea4bba45
newra <newram098@gmail.com>
2024-04-09 11:51:21 +0330

    Define main activity views in class

--- app/src/main/java/com/ramapps/apkshare/MainActivity.java
+++ app/src/main/java/com/ramapps/apkshare/MainActivity.java
@@ -7,18 +7,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private FloatingActionButton fabSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        init();
+    }
+
+    private void init() {
+        recyclerView = findViewById(R.id.mainRecyclerView);
+        fabSend = findViewById(R.id.mainFloatingActionBarSend);
     }
 }
