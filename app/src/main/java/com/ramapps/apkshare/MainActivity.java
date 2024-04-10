229ae9d774828c4664819d98147f95cfbc3a0c52
newra <newram098@gmail.com>
2024-04-10 17:58:15 +0330

    Implement sorting list items (Name, Install date, Size).

--- app/src/main/java/com/ramapps/apkshare/MainActivity.java
+++ app/src/main/java/com/ramapps/apkshare/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ramapps.apkshare;
 
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.Insets;
@@ -15,6 +17,7 @@ import android.widget.RelativeLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.OnApplyWindowInsetsListener;
 import androidx.core.view.ViewCompat;
@@ -23,18 +26,31 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String PREFERENCES_SETTINGS = "Settings";
+    public static final String PREFERENCES_SETTINGS_SORT_BY = "Sort by";
+
+    public static final int FLAG_SORT_BY_NAME = 0;
+    public static final int FLAG_SORT_BY_INSTALL_DATE = 1;
+    public static final int FLAG_SORT_BY_SIZE = 2;
+
+    private MaterialToolbar toolbar;
     private RecyclerView recyclerView;
     public static FloatingActionButton fabSend;
 
     private List<PackageInfo> installedPackagesInfo;
     private List<Boolean> selectionTracker;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +59,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) getWindow().setNavigationBarColor(0x88FFFFFF);
         init();
+        setSupportActionBar(toolbar);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         getInstalledApps();
+        sortPackageInfoList();
         showAppsOnScreen();
+    }
+
+    private void sortPackageInfoList() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            installedPackagesInfo.sort(new Comparator<PackageInfo>() {
+                @Override
+                public int compare(PackageInfo o1, PackageInfo o2) {
+                    int sortType = preferences.getInt(PREFERENCES_SETTINGS_SORT_BY, FLAG_SORT_BY_NAME);
+                    if (sortType == FLAG_SORT_BY_NAME){
+                        String name1 = getPackageManager().getApplicationLabel(o1.applicationInfo) + "";
+                        String name2 = getPackageManager().getApplicationLabel(o2.applicationInfo) + "";
+                        return name1.compareTo(name2);
+                    } else if (sortType == FLAG_SORT_BY_INSTALL_DATE){
+                        String date1 = o1.firstInstallTime + "";
+                        String date2 = o2.firstInstallTime + "";
+                        return date1.compareTo(date2);
+                    } else if (sortType == FLAG_SORT_BY_SIZE) {
+                        String size1 = new File(o1.applicationInfo.sourceDir).length() + "";
+                        String size2 = new File(o2.applicationInfo.sourceDir).length() + "";
+                        return size1.compareTo(size2);
+                    }
+                    return 0;
+                }
+            });
+        }
     }
 
     private void showAppsOnScreen() {
@@ -68,6 +111,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void init() {
+        preferences = getSharedPreferences(PREFERENCES_SETTINGS, MODE_PRIVATE);
+
+        toolbar = findViewById(R.id.mainToolbar);
         recyclerView = findViewById(R.id.mainRecyclerView);
         fabSend = findViewById(R.id.mainFloatingActionBarSend);
         fabSend.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
@@ -93,7 +139,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (item.getItemId() == R.id.mainMenuItemSort) {
-            //TODO: implement here.
+            AlertDialog dialog = new MaterialAlertDialogBuilder(this)
+                    .setTitle(R.string.sort_by)
+                    .setSingleChoiceItems(R.array.sortOptions, preferences.getInt(PREFERENCES_SETTINGS_SORT_BY, FLAG_SORT_BY_NAME), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            preferences.edit().putInt(PREFERENCES_SETTINGS_SORT_BY, which).apply();
+                            sortPackageInfoList();
+                            showAppsOnScreen();
+                            dialog.dismiss();
+                        }
+                    })
+                    .create();
+            dialog.show();
         } else if (item.getItemId() == R.id.mainMenuItemType) {
             //TODO: implement here.
         } else if (item.getItemId() == R.id.mainMenuItemColumnCount) {
