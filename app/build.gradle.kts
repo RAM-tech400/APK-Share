d47e2b8aa9705333b0ead8196e2a9929b087c547
newra <newram098@gmail.com>
2024-04-25 09:45:33 +0330

    Change version name to "0.9.9 (Stage)".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "0.97 (Trial)"
+        versionName = "0.9.9 (Stage)"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
