62a3327427cd8f4ecb7ba3db2bffbd1cbf941074
Reza Alimohammadi <newram098@gmail.com>
2025-11-06 23:34:07 +0330

    Do cleanning codes.

--- app/src/main/java/com/ramapps/apkshare/AboutActivity.java
+++ app/src/main/java/com/ramapps/apkshare/AboutActivity.java
@@ -1,13 +1,9 @@
 package com.ramapps.apkshare;
 
-import static com.ramapps.apkshare.GlobalVariables.*;
-
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +12,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.button.MaterialButton;
 
@@ -32,25 +27,28 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_about);
-        init();
+        initViews();
         addListeners();
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.about), (v, insets) -> {
-            Insets displayCutouts = insets.getInsets(WindowInsetsCompat.Type.displayCutout());
-            findViewById(R.id.aboutNestedScrollView).setPadding(
-                    displayCutouts.left,
-                    findViewById(R.id.aboutNestedScrollView).getPaddingTop(),
-                    displayCutouts.right,
-                    findViewById(R.id.aboutNestedScrollView).getPaddingBottom());
-            toolbar.setPadding(
-                    displayCutouts.left,
-                    toolbar.getPaddingTop(),
-                    displayCutouts.right,
-                    toolbar.getPaddingBottom());
-            return insets;
-        });
     }
 
     private void addListeners() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.about), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            Insets displayCutouts = insets.getInsets(WindowInsetsCompat.Type.displayCutout());
+            int leftInset = Math.max(systemBars.left, displayCutouts.left);
+            int rightInset = Math.max(systemBars.right, displayCutouts.right);
+            findViewById(R.id.aboutNestedScrollView).setPadding(
+                    leftInset,
+                    findViewById(R.id.aboutNestedScrollView).getPaddingTop(),
+                    rightInset,
+                    findViewById(R.id.aboutNestedScrollView).getPaddingBottom());
+            toolbar.setPadding(
+                    leftInset,
+                    toolbar.getPaddingTop(),
+                    rightInset,
+                    toolbar.getPaddingBottom());
+            return insets;
+        });
         btnGithub.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,7 +60,7 @@ public class AboutActivity extends AppCompatActivity {
         });
     }
 
-    private void init() {
+    private void initViews() {
         appBarLayout = findViewById(R.id.aboutAppBarLayout);
         toolbar = findViewById(R.id.aboutToolbar);
         btnGithub = findViewById(R.id.aboutButtonGithub);
