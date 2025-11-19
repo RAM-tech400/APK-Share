880f944132ef5f5070e042a0e0f342eb69617cd4
Reza Alimohammadi <newram098@gmail.com>
2025-11-19 19:43:40 +0330

    Improvements for PermissionListModalBottomSheet

--- app/src/main/java/com/ramapps/apkshare/PermissionsListModalBottomSheet.java
+++ app/src/main/java/com/ramapps/apkshare/PermissionsListModalBottomSheet.java
@@ -1,39 +1,56 @@
 package com.ramapps.apkshare;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 public class PermissionsListModalBottomSheet extends BottomSheetDialogFragment {
+    public static final String TAG = "PermissionsListModalBottomSheet";
 
-    @Nullable
+    private RelativeLayout relativeLayoutStoragePermission;
+
+    @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.bottom_sheet_permissions_list, container, false);
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View contentView = LayoutInflater.from(requireContext()).inflate(R.layout.bottom_sheet_permissions_list, null);
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(requireContext());
+        bottomSheetDialog.setContentView(contentView);
+        initComponents(contentView);
+        addListeners();
+        return bottomSheetDialog;
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    private void initComponents(View contentView) {
+        relativeLayoutStoragePermission = contentView.findViewById(R.id.permissionsBottomSheetRelativeLayoutStoragePermission);
+    }
 
-        RelativeLayout relativeLayoutStoragePermission = view.findViewById(R.id.permissionsBottomSheetRelativeLayoutStoragePermission);
-        relativeLayoutStoragePermission.setOnClickListener(v -> {
-            Intent intent = new Intent();
-            intent.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
-            intent.setData(Uri.fromParts("package", getContext().getPackageName(), null));
-            startActivity(intent);
+    private void addListeners() {
+        relativeLayoutStoragePermission.setOnClickListener((v) -> {
+            Intent intentStoragePermissionDetails = new Intent();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                intentStoragePermissionDetails.setAction(Settings.ACTION_MANAGE_APP_ALL_FILES_ACCESS_PERMISSION);
+            } else {
+                intentStoragePermissionDetails.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            }
+            intentStoragePermissionDetails.setData(Uri.fromParts("package", requireContext().getPackageName(), null));
+            startActivity(intentStoragePermissionDetails);
         });
-
     }
 
 }
